c590565a10994b75d13865470503e0b226900eb6
lu1kaifeng <8302078+lu1kaifeng@user.noreply.gitee.com>
2021-06-16 08:33:13 +0800

    new layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,22 @@
 <template>
   <v-container class="">
     <v-row no-gutters>
-      <v-col class="d-flex justify-center" >
-        <v-img max-width="25em" :src="require('../../public/Picture1.png')"/>
-      </v-col>
-      <v-col class="d-flex justify-center">
-        <v-img max-width="25em" :src="require('../../public/Picture2.png')"/>
-      </v-col>
-    </v-row>
-
-    <v-row no-gutters>
       <v-col cols="12">
         <p class="text-justify text-subtitle-1" >
-          Nuclear insertions of mitochondrial origin (NUMTs) can be useful tools in evolution and population studies. However, due to their similarity to mtDNA, NUMTs may also be a source of contamination in mtDNA studies. The main goal of this work is to present a database of NUMTs, based on the previously reported studies and the  latest version of the human genome-GRCh38 draft. A total of 783 insertions were identified. There are 33 paralogous sequences with over 80% sequence similarity and of a greater length than 500bp.
+          <strong>Nuclear insertions of mitochondrial origin (NUMTs)</strong> can be useful tools in evolution and population studies. However, due to their similarity to mtDNA, NUMTs may also be a source of contamination in mtDNA studies. The main goal of this work is to present a database of NUMTs, based on the previously reported studies and the  latest version of the human genome-GRCh38 draft. A total of 783 insertions were identified. There are 33 paralogous sequences with over 80% sequence similarity and of a greater length than 500bp.
         </p>
       </v-col>
     </v-row>
+    <v-row no-gutters>
+      <v-col class="d-flex justify-center" >
+        <v-img max-width="25em" :src="require('../../public/Picture1.png')"/>
+      </v-col>
+      <v-col class="d-flex justify-center" >
+        <v-img max-width="25em" :src="require('../../public/Picture3.jpg')"/>
+      </v-col>
+    </v-row>
+
+
   </v-container>
 </template>
 
